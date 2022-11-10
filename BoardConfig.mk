910d27357a18e1b9463f4187131f6727bfd9c38e
Marcel <marcel6260@gmail.com>
2022-11-10 15:16:55 +0200

    Revert "add vulkan as default renderer"
    This reverts commit 3441afff295c9c3aa98ed77d1e876431a2033679 because vulkan is giving us some random garbage graphics.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -208,6 +208,3 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/configs/power/mode-ext.cpp
 
 # Inherit the proprietary files
 -include vendor/nokia/NB1/BoardConfigVendor.mk
-
-# add vulkan
-TARGET_USES_VULKAN := true
